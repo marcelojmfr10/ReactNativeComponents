151139d02d2ea1aaee889524eeb405906beec003
Marcelo Fuentes <mfuentes@fedecovera.com>
2026-08-24 11:55:06 -0600

    Alerts y TextInput

--- presentation/shared/ThemedCard.tsx
+++ presentation/shared/ThemedCard.tsx
@@ -5,9 +5,10 @@ interface Props extends ViewProps {
   className?: string;
 }
 
-const ThemedCard = ({ className, children }: Props) => {
+const ThemedCard = ({ className, children, ...rest }: Props) => {
   return (
     <View
+      {...rest}
       className={`bg-white dark:bg-black/10 rounded-xl p-2 shadow shadow-black/5 ${className}`}
     >
       {children}
