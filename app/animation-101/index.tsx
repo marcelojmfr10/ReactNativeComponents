ab70342f3439f83a3c3065d6eb000d94d8c64cce
Marcelo Fuentes <mfuentes@fedecovera.com>
2026-08-24 09:36:54 -0600

    Animaciones básicas

--- app/animation-101/index.tsx
+++ app/animation-101/index.tsx
@@ -1,10 +1,56 @@
-import { View, Text } from 'react-native';
+import ThemedButton from "@/presentation/shared/ThemedButton";
+import ThemedView from "@/presentation/shared/ThemedView";
+import { useRef } from "react";
+import { Animated, Easing } from "react-native";
 
 const Animation101Screen = () => {
+  const animatedOpacity = useRef(new Animated.Value(0)).current;
+  const animatedTop = useRef(new Animated.Value(-100)).current;
+
+  const fadeIn = () => {
+    Animated.timing(animatedOpacity, {
+      toValue: 1,
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+
+    Animated.timing(animatedTop, {
+      toValue: 0,
+      duration: 700,
+      useNativeDriver: true,
+      // easing: Easing.elastic(3),
+      easing: Easing.bounce,
+    }).start();
+  };
+
+  const fadeOut = () => {
+    Animated.timing(animatedOpacity, {
+      toValue: 0,
+      duration: 300,
+      useNativeDriver: true,
+      // }).start(() => animatedTop.setValue(-100));
+    }).start(() => animatedTop.resetAnimation());
+  };
+
   return (
-    <View>
-      <Text>Animation101Screen</Text>
-    </View>
+    <ThemedView margin className="justify-center items-center flex-1">
+      <Animated.View
+        className="bg-light-secondary dark:bg-dark-secondary rounded-xl"
+        style={{
+          width: 150,
+          height: 150,
+          opacity: animatedOpacity,
+          transform: [{ translateY: animatedTop }],
+        }}
+      />
+
+      <ThemedButton className="my-5" onPress={fadeIn}>
+        FadeIn
+      </ThemedButton>
+      <ThemedButton className="my-5" onPress={fadeOut}>
+        FadeOut
+      </ThemedButton>
+    </ThemedView>
   );
 };
 export default Animation101Screen;
